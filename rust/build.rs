1ee97e6bbd55618bc74edc9a0c30d9cedd699f6d
Nitesh Balusu <niteshbalusu@icloud.com>
2024-09-15 10:03:04 -0400

    update build.rs to handle all OS's

--- rust/build.rs
+++ rust/build.rs
@@ -8,18 +8,26 @@ fn main() {
     );
     println!("cargo:rustc-link-lib=static=lnd");
     println!("cargo:rerun-if-changed=../liblnd.h");
-    println!("cargo:rustc-link-lib=resolv");
 
+    // Platform-specific configurations
     if cfg!(target_os = "macos") {
         println!("cargo:rustc-link-lib=framework=CoreFoundation");
         println!("cargo:rustc-link-lib=framework=Security");
+        println!("cargo:rustc-link-lib=resolv");
     } else if cfg!(target_os = "windows") {
-        // Add Windows-specific configurations here if needed
+        println!("cargo:rustc-link-lib=ws2_32");
+        println!("cargo:rustc-link-lib=userenv");
+        println!("cargo:rustc-link-lib=crypt32");
+        println!("cargo:rustc-link-lib=advapi32");
+    } else {
+        // Linux and other Unix-like systems
+        println!("cargo:rustc-link-lib=resolv");
     }
 
     let bindings = bindgen::Builder::default()
         .header("../liblnd.h")
         .allowlist_file("../liblnd.h")
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
         .generate()
         .expect("Unable to generate bindings");
 
